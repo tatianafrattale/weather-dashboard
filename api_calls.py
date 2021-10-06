37932d9004dcf793d67a14f064ac8296ec1cb734
tatianafrattale <tatianafrattale@gmail.com>
2021-10-06 19:51:02 -0400

    final code(hopefully)- switch flask to api calls

--- api_calls.py
+++ api_calls.py
@@ -3,19 +3,5 @@ import weather_data
 import seven_day_forecast
 
 # API functions
-def getData():
-    with open('weather_data.json', 'r') as outfile:
-        current_data1 = json.load(outfile)  
-    return current_data1
-
-def getData2():
-    with open('seven_day_weather_data.json', 'r') as outfile:
-        seven_day_data1 = json.load(outfile)
-    return seven_day_data1
-
-current_data1 = getData()
-seven_day_data1 = getData2()
-
-# CHANGE BACK TO THIS CODE TO MAKE FLASK READ FROM API AGAIN
-# current_data1 = weather_data.makecall()
-# seven_day_data1 = seven_day_forecast.make7daycall()
+current_data1 = weather_data.makecall()
+seven_day_data1 = seven_day_forecast.make7daycall()
